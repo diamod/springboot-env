3a60d64ec3bad1100bc33884e2343d3842e98474
fanwh26365 <fanwh26365@hundsun.com>
2020-04-15 17:31:17 +0800

    commit

---
+++ src/main/java/com/test/AppStarter.java
@@ -0,0 +1,18 @@
+package com.test;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+* @Author:         Janes
+* @Description:    springboot annotation
+* @CreateDate:     2020/4/10 13:07
+* @Version:        1.0
+*/
+@SpringBootApplication
+public class AppStarter
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
